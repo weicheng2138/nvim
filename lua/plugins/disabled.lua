3defdd538d0fa8fdca63af6d91bc1d09593e0523
WEICHENG HUNG <weichenghung@WEICHENGdeMacBook-Air.local>
2025-05-12 22:47:20 +0800

    chore: init and setup some basic config

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+}
