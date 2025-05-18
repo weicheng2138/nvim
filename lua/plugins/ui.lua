d034e8638ec55fb6537ae1d63006c9c6dc83a65f
weicheng2138 <weicheng2138@gmail.com>
2025-05-18 15:22:37 +0800

    feat: switch ctrl+space to ctrl+j to trigger code complete

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,5 +1,17 @@
 return {
   {
+    "Saghen/blink.cmp",
+    opts = {
+      keymap = {
+        -- Remove the default Ctrl+Space mapping if present
+        ["<C-Space>"] = nil,
+        -- Map Ctrl+j to trigger completion
+        ["<C-j>"] = { "show" },
+        -- You can keep or customize other keymaps as needed
+      },
+    },
+  },
+  {
     "nvimdev/dashboard-nvim",
     event = "VimEnter",
     opts = function(_, opts)
